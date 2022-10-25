da654e2267ba00da3f90ccaa824888771abd1052
Levi Van Veen <levivanveen2@gmail.com>
2022-10-25 14:18:40 -0400

    A3 Init

---
+++ A3/sudoku.c
@@ -0,0 +1,24 @@
+/**
+-------------------------------------------------
+Authors:    Levi Van Veen
+ID:         200852490
+Email:      vanv2490@mylaurier.ca
+Version     25-10-2022
+-------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    // Confirm user gave args
+    if (argc <= 1) {
+        puts("Insufficient parameters passed");
+        return 1;
+    }
+
+    // Read the sudodu file
+
+    return 0;
+}
