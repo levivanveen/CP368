bd651735d33988ebb5f48c6ddfe4217bb227272e
Levi Van Veen <levivanveen2@gmail.com>
2022-11-04 12:27:09 -0400

    Worked on fcfs

--- A3/fcfs.c
+++ A3/fcfs.c
@@ -12,7 +12,9 @@ typedef struct threadInfo{
     int turn_around_time;
 } threadInfo;
 
-threadInfo* readThreads(char* in_file, int* thread_arr_size);
+threadInfo* readThreads(char* in_file, int *thread_arr_size);
+void calcTurnaround(threadInfo* thread_arr, int thread_arr_size);
+void calcWait(threadInfo* thread_arr, int thread_arr_size);
 
 int main(int argc, char* argv[]) {
     // Confirm user gave args
@@ -28,22 +30,53 @@ int main(int argc, char* argv[]) {
     strcat(in_file, "./");
     strcat(in_file, argv[1]);
 
-    int* thread_arr_size;
-    threadInfo* thread_arr = readThreads(in_file, thread_arr_size);
+    int thread_arr_size = 0;
+    threadInfo* thread_arr = readThreads(in_file, &thread_arr_size);
 
-    printf("%d\n", thread_arr_size);
-    for (int i = 0; i < 8; i++) {
+    calcTurnaround(thread_arr, thread_arr_size);
+
+    // Need to 
+    for (int i = 0; i < thread_arr_size; i++) {
         printf("\n%d %d %d\n", thread_arr[i].p_id, thread_arr[i].arr_time, thread_arr[i].burst_time);
     }
 
+    // Clean up allocated stuff and return
     free(thread_arr);
     thread_arr = NULL;
     return 0;
 }
 
-threadInfo* readThreads(char* in_file, int* thread_arr_size) {
+void calcTurnaround(threadInfo* thread_arr, int thread_arr_size) {
+    /*  To calculate the turnaround time
+        Do a while loop and for each thread and check when it is done
+     */
+    int finished = 0;
+    int time = 0;
+    int wait_queue[thread_arr_size];
+    int counter = 0;
+
+    while (!finished) {
+        printf("wow");
+        // First, we check if any new threads have arrived
+        for (int i = 0; i < thread_arr_size; i++) {
+            if (thread_arr[i].arr_time == time) {
+                wait_queue[i] = thread_arr[i].burst_time;
+                counter++;
+            }
+        }
+        if (counter == 6) {
+            printf("Done\n");
+            finished = 1;
+        }
+    }
+    for (int i = 0; i < 6; i++) {
+        printf("ok %d\n", wait_queue[i]);
+    }
+}
+
+threadInfo* readThreads(char* in_file, int *thread_arr_size) {
     const int buffer_size = 100;
-    int in_desc, rd, col = 1, thread_index = 1, num;
+    int in_desc, rd, col = 1, thread_index = 1, num = 0;
     char* token;
     char delimit[] = " \n,";
     char *c = malloc(buffer_size*sizeof(char));
@@ -62,7 +95,7 @@ threadInfo* readThreads(char* in_file, int* thread_arr_size) {
         while (token != NULL) {
             int i = 0;
             while (token[i] != '\0') {
-                if(((token[i] - 48) >=0) && (token[i] - 48 <=9))
+                if(((token[i] - 48) >= 0) && (token[i] - 48 <= 9))
                     num = (num*10) + (token[i] - 48);
                 i++;
             }
@@ -91,16 +124,13 @@ threadInfo* readThreads(char* in_file, int* thread_arr_size) {
             num = 0;
             token = strtok(NULL, delimit);
         }
-
         // Overwrite c buffer so last write doesn't repeat chars
         for (int i = 0; i < buffer_size; i++) {
             c[i] = '\0';
         }
     }
-
     free(c);
     c = NULL;
-
-    thread_arr_size = thread_index;
+    *thread_arr_size = thread_index - 1;
     return thread_arr;
 }
